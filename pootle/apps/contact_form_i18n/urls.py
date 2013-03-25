fa8f9aef0e2e4dc1fc73d707bc465835c68c6a23
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-03-25 15:41:32 +0100

    django-1.5: direct_to_template => TemplateView.as_view
    One piece of documentation was changed in the process.

--- pootle/apps/contact_form_i18n/urls.py
+++ pootle/apps/contact_form_i18n/urls.py
@@ -12,17 +12,18 @@ hierarchy (for best results with the defaults, include it under
 
 
 from django.conf.urls import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from contact_form_i18n.views import contact_form
 
 
-urlpatterns = patterns('',
-                       url(r'^/?$',
-                           contact_form,
-                           name='contact_form'),
-                       url(r'^sent/?$',
-                           direct_to_template,
-                           { 'template': 'contact_form/contact_form_sent.html' },
-                           name='contact_form_sent'),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^/?$',
+        contact_form,
+        name='contact_form'),
+    url(r'^sent/?$',
+        TemplateView.as_view(
+            template_name='contact_form/contact_form_sent.html'),
+        name='contact_form_sent'),
+    )
